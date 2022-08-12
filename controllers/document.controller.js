a08dd93d4e8c3aa2cb344b5f5795f1a86a717a2e
Musabii Mambetov <musabi@live.com>
2022-08-12 12:59:08 +0300

    get title with id

--- controllers/document.controller.js
+++ controllers/document.controller.js
@@ -5,14 +5,23 @@ const db = require("./../db");
 class DocumentController {
   async getDocument(req, res) {
     const { id } = req.params;
-    const result = await db.query(`select * from document where id = $1`, [id]);
+    const result = await db.query(`
+    select *, (select title from client c where c.id = d.client_id) client_title,
+    (select title from service s where s.id = d.service_id) service_title
+    from document d
+    where id = $1`, [id]);
     res.json(result.rows[0]);
   }
 
   async getDocuments(req, res) {
     const { filter, order } = req.body;
     console.log(filter);
-    let query = `select * from document`;
+    let query = `
+    select *, (select title from client c where c.id = d.client_id) client_title,
+    (select title from service s where s.id = d.service_id) service_title
+    from document d
+    `;
+
     if (filter?.value) {
       query = `${query} where ${filter.column} ${filter.compare} '${filter.value}'`;
       console.log(query);
@@ -27,12 +36,14 @@ class DocumentController {
   }
 
   async createDocument(req, res) {
-    const { date,client_id,service_id } = req.body;
+    const { date, client_id, service_id } = req.body;
     const result = await db.query(
       `
     INSERT INTO
     document (date, client_id, service_id)
-    values ($1,$2,$3)`, [date, +client_id, +service_id]);
+    values ($1,$2,$3)`,
+      [date, +client_id, +service_id]
+    );
     res.send("ok");
   }
 
