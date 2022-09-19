ea27c19de9ada4a6a064d73e7e02d73cb9d294b8
Musabii Mambetov <musabi@live.com>
2022-09-19 16:42:32 +0300

    options response def null

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -88,8 +88,8 @@ class ClientController {
     const {
       title,
     } = req.query;
-    if (!title)  res.status(200).send({});
-    const query = `select id as value,title as text from client where title like '${title}%'`;
+    if (!title)  res.status(200).send(null);
+    const query = `select id as value,title as text from client where title like '${title}%' limit 10`;
     const result = await db.query(query);
     res.status(200).send(result.rows);
   }
